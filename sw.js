4d45f013cab4649f177edf386d08e4c5411b3663
pablofernandez1983-cyber <pablofernandez1983@gmail.com>
2026-06-09 19:32:01 -0300

    Bumpa SW a v4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mundial2026-v3';
+const CACHE = 'mundial2026-v4';
 const ASSETS = ['./', './index.html', './icon-192.png', './icon-512.png', './manifest.json'];
 
 self.addEventListener('install', e => {
